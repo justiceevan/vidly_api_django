ff6db2af8abac7e2abf9dfe10f0a3c13d6447704
Justice Evans <justiceevansmwema@gmail.com>
2022-09-01 22:58:02 +0300

    Created genres and movies models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
+    numberInStock = models.IntegerField(default=0, blank=True, null=True)
+    dailyRentalRate = models.DecimalField(
+        max_digits=4, decimal_places=1, blank=True, null=True)
+
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
