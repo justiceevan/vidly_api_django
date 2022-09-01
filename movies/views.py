0db3ad95a2ce0bfa7459d2dd7f2d3a5c595beb12
Justice Evans <justiceevansmwema@gmail.com>
2022-09-01 23:20:49 +0300

    Created a Genre serializer and a getGenres view to get all the genres from the database

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Genre
+from .serializer import GenreSerializer
 
 # Create your views here.
+
+
+def getGenres(request):
+    genres = Genre.objects.all()
+    serializer = GenreSerializer(genres, many=True)
+    return Response(serializer.data)
