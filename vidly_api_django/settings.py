a8f3ee18acaa4686cade738e1dd6bda4cce55e35
Justice Evans <justiceevansmwema@gmail.com>
2022-09-01 22:51:34 +0300

    Created a movies app and registered it to the INSTALLED APPS together with the rest_framework module

--- vidly_api_django/settings.py
+++ vidly_api_django/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    'rest_framework',
+
+    'movies.apps.MoviesConfig',
 ]
 
 MIDDLEWARE = [
@@ -88,9 +92,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
 ]
 
 
