b3ea1d37e78ea0993012f98b344f9fda816bb14a
Justice Evans <justiceevansmwema@gmail.com>
2022-09-17 08:34:45 +0300

    Added the user login url

--- movies/urls.py
+++ movies/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('movies/new/', views.createMovie, name='create-movie'),
     path('movies/<str:pk>/', views.getMovie, name='movie'),
     path('movies/<str:pk>/update/', views.updateMovie, name='update-movie'),
-    path('movies/<str:pk>/delete/', views.deleteMovie, name='delete-movie')
+    path('movies/<str:pk>/delete/', views.deleteMovie, name='delete-movie'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(),
+         name='token-obtain-pair'),
 ]
