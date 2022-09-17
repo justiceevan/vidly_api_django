ffe1533b30e9e07912ddb844e2844f0c2c9f7d3b
Justice Evans <justiceevansmwema@gmail.com>
2022-09-17 08:32:45 +0300

    Added UserLoginView

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .models import Genre, Movie
-from .serializer import GenreSerializer, MovieSerializer
+from .serializer import GenreSerializer, MovieSerializer, UserSerializer, UserSerializerWithToken
 
 # Create your views here.
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
